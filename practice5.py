26cf34f46ff475b445465449d7ed647624645c24
Nira khanal <khanalnira01@gmail.com>
2025-10-15 14:41:12 +0545

    Practice of dictionary and sets

---
+++ practice5.py
@@ -0,0 +1,14 @@
+#1.Write a program to create a dictionary of hindi words with values as 
+#their english translation. Provide user with an option to look it up!
+#Ans
+words = {
+    "madad": "Help",
+    "kursi": "chair",
+    "billi": "cat"
+}
+word = input("Enter the word you want meaning of: ")
+print(words[word])
+
+#2.Write a program to input eight numbers from the user and display 
+# all the unique numbers(once).
+#Ans
