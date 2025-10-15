73eeeb60bc2ca8154069e3de4ae36ba2ff4c0b5c
Nira khanal <khanalnira01@gmail.com>
2025-10-15 09:13:29 +0545

    Sets

---
+++ sets.py
@@ -0,0 +1,10 @@
+# #d = {} #This is empty dictionary
+s = {1,2,4,6,1,32,87}
+# e = set() #donot use s={} as it will create an empty dictionary
+# print(e)
+
+# s.add(566)
+# print(s,type(s))
+# print(len(s))
+s.remove(1)
+print(s,type(s))
